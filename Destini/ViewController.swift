d7d4889fa990a07fdfd4ea954f88f13bf885b92f
luismgb <luismigelgb@gmail.com>
2017-12-25 00:41:15 -0500

    Add navigating through story functionality

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -10,54 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    var currentSectionIndex: Int = 0
     let allStories = StoryCollection()
     
-    // UI Elements linked to the storyboard
+    
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
-    
-    // TODO Step 5: Initialise instance variables here
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyTextView.text = allStories.array[0].story
-        topButton.setTitle(allStories.array[0].answerA, for: .normal)
-        bottomButton.setTitle(allStories.array[0].answerA, for: .normal)
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        updateUI()
         
     }
-
+    
     
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-//        if storyTextView.text == story1 {
-//            if sender.tag == 1 {
-//                storyTextView.text = story3
-//                topButton.setTitle(answer3a, for: .normal)
-//                bottomButton.setTitle(answer3b, for: .normal)
-//            } else if sender.tag == 2 {
-//                storyTextView.text = story2
-//                topButton.setTitle(answer2a, for: .normal)
-//                bottomButton.setTitle(answer2b, for: .normal)
-//            }
-//        }
-//        else if storyTextView.text == story2 {
-//            if sender.tag == 1 {
-//                storyTextView.text = story3
-//            }
-//        }
         
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
-    
+        switch currentSectionIndex {
+        case 0:
+            if sender.tag == 1 {
+                currentSectionIndex = 2
+            } else if sender.tag == 2 {
+                currentSectionIndex = 1
+            }
+        case 1:
+            if sender.tag == 1 {
+                currentSectionIndex = 2
+            } else if sender.tag == 2 {
+                currentSectionIndex = 3
+            }
+        case 2:
+            if sender.tag == 1 {
+                currentSectionIndex = 5
+            } else if sender.tag == 2 {
+                currentSectionIndex = 4
+            }
+        default:
+            break
+        }
+
+        updateUI()
+
     }
     
-
-
-
+    
+    func updateUI() {
+        storyTextView.text = allStories.array[currentSectionIndex].story
+        
+        topButton.setTitle(allStories.array[currentSectionIndex].answerA, for: .normal)
+        bottomButton.setTitle(allStories.array[currentSectionIndex].answerB, for: .normal)
+    }
+    
+    
 }
 
